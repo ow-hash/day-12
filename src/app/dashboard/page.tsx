88f2bd4671cb2302123b7ff9627d2efd42468de9
thanapon <thanapon.won@baac.or.th>
2025-05-15 16:08:58 +0700

    Add Dashboard component with basic layout

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+const Dashboard = () => {
+
+
+    return (
+        <div className='grid grid-rows-[20px_1fr_20px] items-center justify-center mt-20'>
+            Dashboard Page
+        </div>
+    )
+}
+
+
+export default Dashboard
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center">
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-center mt-20">
             <h1>Hello World</h1>
         </div>
     );
