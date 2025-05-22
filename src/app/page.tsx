d02d4267277f78916924af0fd2aff51ab5ed41d0
thanapon <thanapon.won@baac.or.th>
2025-05-22 14:21:00 +0700

    feat: Replace "Hello World" with a link to the dashboard in the Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 
 
 
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <h1>Hello World</h1>
+            <Link href="/dashboard/chart" className="bg-[#facc15] px-5 py-3 m-5 hover:bg-amber-200 rounded-2xl hover:text-black text-white font-bold">To Dashboard</Link>
         </div>
     );
 }
